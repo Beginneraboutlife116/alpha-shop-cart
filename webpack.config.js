1858d7cf9c2723e64ae75c6ec5944c14c1942047
Beginneraboutlife116 <pythonforeye@gmail.com>
2021-09-24 12:21:39 +0800

    feat: project init

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/entry.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'images/[hash].png'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+        ]
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin({
+    template: 'index.html'
+  })],
+  devtool: 'source-map'
+};
