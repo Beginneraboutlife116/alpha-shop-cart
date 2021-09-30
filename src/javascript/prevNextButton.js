0e70f0e1b315cee3405de1e1ba40f6694fb6ff88
Beginneraboutlife116 <pythonforeye@gmail.com>
2021-09-30 21:10:24 +0800

    fix: optimize button flow code

--- src/javascript/prevNextButton.js
+++ src/javascript/prevNextButton.js
@@ -2,23 +2,26 @@ const btn = document.querySelector(".btn")
 const forms = document.querySelectorAll('.content__form-part')
 const btnPrev = document.querySelector('.btn__prev')
 const btnNext = document.querySelector('.btn__next')
+const steps = document.querySelectorAll('.content__stepper-step')
 
-let formPart = 0
+let stepCount = 0
 
 function buttonClickControlForm(e) {
   e.preventDefault()
   const target = e.target.closest('button')
-  let presentForm = formPart
+  let presentStepCount = stepCount
   
   if (target.innerHTML === "確認下單") { return }
   if (target.classList.contains('btn__next')) {
-    formPart += 1
-    toggleDisplayNone(presentForm, formPart)
+    stepCount += 1
+    toggleFormsDisplay(presentStepCount, stepCount)
+    controlStepsStyle(presentStepCount, stepCount)
     btnPrev.classList.remove('disabled')
     modifyBtn()
   } else if (target.classList.contains('btn__prev')) {
-    formPart -= 1
-    toggleDisplayNone(presentForm, formPart)
+    stepCount -= 1
+    toggleFormsDisplay(presentStepCount, stepCount)
+    controlStepsStyle(presentStepCount, stepCount)
     modifyBtn()
     btnNext.innerHTML = `
       <span>下一步</span>
@@ -27,16 +30,28 @@ function buttonClickControlForm(e) {
   }
 }
 
-function toggleDisplayNone (presentNum, futureNum) {
+function toggleFormsDisplay (presentNum, futureNum) {
   forms[presentNum].classList.toggle('d-none')
   forms[futureNum].classList.toggle('d-none')
 }
 
+function controlStepsStyle (presentNum, futureNum) {
+  if (presentNum < futureNum) {
+    steps[presentNum].classList.remove('active')
+    steps[presentNum].classList.add('finished')
+    steps[futureNum].classList.add('active')
+  } else {
+    steps[presentNum].classList.remove('active')
+    steps[futureNum].classList.remove('finished')
+    steps[futureNum].classList.add('active')
+  }
+}
+
 function modifyBtn () {
-  if (formPart === 0) {
+  if (stepCount === 0) {
     return btnPrev.classList.add('disabled')
   }
-  if (formPart === 2) {
+  if (stepCount === 2) {
     return btnNext.innerHTML = "確認下單"
   }
 }
