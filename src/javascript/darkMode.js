3947c88bed590d1dda2e56851ba47948cf54abb3
Beginneraboutlife116 <pythonforeye@gmail.com>
2021-10-04 20:28:36 +0800

    refacotr/refactor header BEM and scss structure

--- src/javascript/darkMode.js
+++ src/javascript/darkMode.js
@@ -1,17 +1,17 @@
 let darkMode = localStorage.getItem('darkMode')
-const darkModeToggle = document.querySelector('#dark-mode-toggle')
+const darkModeToggle = document.querySelector('#darkModeToggle')
 
 if (darkMode === "on") {
-  document.body.classList.add('darkmode')
+  document.body.classList.add('darkMode')
 }
 
 function clickOnDarkModeToggle () {
   darkMode = localStorage.getItem('darkMode')
   if (darkMode !== 'on') {
-    document.body.classList.add('darkmode')
+    document.body.classList.add('darkMode')
     localStorage.setItem('darkMode', 'on')
   } else {
-    document.body.classList.remove('darkmode')
+    document.body.classList.remove('darkMode')
     localStorage.setItem('darkMode', 'off')
   }
 }
